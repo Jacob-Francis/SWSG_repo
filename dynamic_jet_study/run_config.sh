b0cafe50de372e1d7f5fde83524e90a86097c74c
jacob <jjf817@ic.ac.uk>
2025-02-03 15:03:53 +0000

    runnning

--- dynamic_jet_study/run_config.sh
+++ dynamic_jet_study/run_config.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# ReRuns: 15/01/2025
-# nohup python3 sim_study.py 0 0.02 > smaller.out 2> smaller.err &
+nohup python3 sim_study.py 4 0.02 0 3 > methodcompare_0.out 2> methodcompare_0.err
+nohup python3 sim_study.py 3 0.02 0.0001 3 > methodcompare_mid.out 2> methodcompare_mid.err
 
-nohup python3 sim_study.py 2 0.02 '' > nolloydsmaller.out 2> nolloydsmaller.err
-nohup python3 sim_study.py 2 0.01 '' > nolloydsmall.out 2> nolloydsmall.err
-nohup python3 sim_study.py 2 0.005 '' > nolloydmid.out 2> nolloydmid.err
+nohup python3 sim_study.py 7 0.01 0 1 > mid100_0.out 2> mid100_0.err
+nohup python3 sim_study.py 6 0.01 0.0001 1 > mid100_strong.out 2> mid100_strong.err
+nohup python3 sim_study.py 5 0.01 0.00005 1 > mid100_mid.out 2> mid100_mis.err
 
-nohup python3 sim_study.py 2 0.01 'lloyd' > small.out 2> small.err
-nohup python3 sim_study.py 2 0.005 'lloyd' > mid.out 2> mid.err
+nohup python3 sim_study.py 2 0.0025 0.00005 1 > hifi.out 2> hifi.err
 
-
+# nohup python3 sim_study.py 5 0.005 0 > nolloydmid.out 2> nolloydmid.err
+# parser.add_argument("cuda", type=int, help="cuda index")
+# parser.add_argument("epsilon", type=float, help="size of epsilon")
+# parser.add_argument("strength", type=float, help="strength of perturbation")
 
